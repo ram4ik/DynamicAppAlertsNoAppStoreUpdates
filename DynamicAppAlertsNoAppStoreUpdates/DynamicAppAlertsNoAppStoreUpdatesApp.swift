0afd63f84551daf1d1c84555d7cbf8315a59954d
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-01 12:26:17 +0200

    Initial Commit

---
+++ DynamicAppAlertsNoAppStoreUpdates/DynamicAppAlertsNoAppStoreUpdatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicAppAlertsNoAppStoreUpdatesApp.swift
+//  DynamicAppAlertsNoAppStoreUpdates
+//
+//  Created by test on 01.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DynamicAppAlertsNoAppStoreUpdatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
